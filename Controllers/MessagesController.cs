f83bdf9dfaf51a21e62474a7290f769f9e725645
金石开 <Skyjin@outlook.com>
2017-07-11 15:31:57 +0800

    tonight

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -8,7 +8,7 @@ using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using Microsoft.Bot.Builder.Dialogs;
-//using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Builder.Luis;
 using System.Collections.Generic;
 using System.Text;
